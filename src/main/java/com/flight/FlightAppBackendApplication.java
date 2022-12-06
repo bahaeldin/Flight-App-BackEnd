3063c141f70fa109d0a0b650acd28a3a1cd5a6f5
BAHAELDIN BAHAR <36114314+bahaeldin@users.noreply.github.com>
2022-12-07 00:30:34 +0200

    project setup

---
+++ src/main/java/com/flight/FlightAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightAppBackendApplication.class, args);
+	}
+
+}
